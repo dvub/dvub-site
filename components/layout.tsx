41d95f75d777ea8565272e51d72c517e1e02b191
dvub <84478450+dvub@users.noreply.github.com>
2022-08-25 11:34:28 -0700

    style post layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,26 +11,33 @@ interface Props {
 const Layout: FC<Props> = (props) => {
 
     const { children, meta } = props;
-    const { title, author, date, authorLink } = meta;
+    const { title, author, date, authorLink, description } = meta;
 
 
+
+    const sideMargin = '12rem';
     const bodyStyle: CSS.Properties = {
         textAlign: 'left',
+        marginLeft: sideMargin,
+        marginRight: sideMargin,
+        marginTop: '2rem',
     }
     return (
         <>
             <Head>
                 <title>{title}</title>
-                <link rel="stylesheet" href="https://unpkg.com/dracula-prism/dist/css/dracula-prism.css"/>
+                <link rel="stylesheet" href="https://unpkg.com/dracula-prism/dist/css/dracula-prism.css" />
             </Head>
 
-            <div> 
+            <div>
                 {/* title, author, date, etc */}
-                <header style={{textAlign: 'center'}}>
+                <header style={bodyStyle}>
                     <h1>{title}</h1>
                     By: <a href={authorLink}>{author}</a>
-                    <br/>
+                    <br />
                     Date: {date}
+                    <br/>
+                    {description}
                 </header>
 
                 {/* props.children will render the standard md content*/}
@@ -38,7 +45,7 @@ const Layout: FC<Props> = (props) => {
 
                     <Link href='../posts'>Back to posts</Link>
 
-                    {children}    
+                    {children}
                 </div>
 
                 {/*footer goes here */}
