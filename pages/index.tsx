dfbd325d1becc9bdd746512c7c78359d8dea6a83
dvub <84478450+dvub@users.noreply.github.com>
2022-09-06 17:44:06 -0700

    work on sphere of points

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { meshBounds, OrbitControls, Stars } from '@react-three/drei'
+import { Canvas, MeshProps, ReactThreeFiber, useFrame } from '@react-three/fiber'
+import { meshBounds, OrbitControls, Point, PointMaterial, Points, Stars } from '@react-three/drei'
 import CSS from 'csstype'
 import * as THREE from 'three'
-import { useRef } from 'react'
+import { Ref, useRef, useState } from 'react'
+import { BufferAttribute, BufferGeometry, PointsMaterial } from 'three'
 // todo:
 // use head to add titles to pages other than posts
 
@@ -19,35 +20,62 @@ const Home: NextPage = () => {
       <div style={{ textAlign: 'center' }}>
         <h1 style={{}}>Welcome</h1>
         <Canvas style={canvasStyle}>
-          <OrbitControls/>
-          <Stars/>
-          <ambientLight intensity={0.5}/>
+          <OrbitControls />
+          <ambientLight intensity={0.5} />
           <spotLight
-            position={[10,15,10]}
+            position={[10, 15, 10]}
             angle={0.3}
           />
-          <Box />
+          <Sphere />
         </Canvas>
       </div>
     </div>
   );
 }
 
-const Box = () => {
-  const mesh = useRef<THREE.Mesh>(null!);
+const Sphere = () => {
+  const pointCount = 1000;
+  const radius = 20;
+  const points = new Float32Array(pointCount * 3);
+  for (let i = 0; i < points.length; i+=3) {
+
+    const point = randomSpherePoint(0,0,0,radius);
+
+    points[i] = point[0];
+    points[i+1] = point[1];
+    points[i+2] = point[2];
+
+  }  const attribute = new BufferAttribute(points, 3);
+  const ref = useRef<BufferAttribute>(attribute);
+
   useFrame(() => {
-    mesh.current.rotation.x += 0.01;
-    mesh.current.rotation.y += 0.01;
   });
 
-  return (
-    <mesh ref={mesh}>
-      
-      <boxBufferGeometry attach="geometry"/>
-      <meshLambertMaterial attach="material" color="hotpink" />
 
-    </mesh>
+  return (
+    <points >
+      <bufferGeometry>
+        <bufferAttribute attach={"attributes-position"} {...attribute} ref={ref}/>
+      </bufferGeometry>
+      <pointsMaterial
+        size={0.1}
+        color={0xff00ff}
+        sizeAttenuation={true}
+      />
+    </points>
+
   );
+}
+
+function randomSpherePoint(x0: number, y0: number, z0: number, radius: number): [number, number, number] {
+  var u = Math.random();
+  var v = Math.random();
+  var theta = 2 * Math.PI * u;
+  var phi = Math.acos(2 * v - 1);
+  var x = x0 + (radius * Math.sin(phi) * Math.cos(theta));
+  var y = y0 + (radius * Math.sin(phi) * Math.sin(theta));
+  var z = z0 + (radius * Math.cos(phi));
+  return [x, y, z];
 }
 
 export default Home
