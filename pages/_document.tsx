3d0eacff9ce72f3b8f97fb7eefe56f824eceb7c3
dvub <84478450+dvub@users.noreply.github.com>
2022-09-24 19:31:20 -0700

    font changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link rel="preconnect" href="https://fonts.gstatic.com"/>
-      <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Roboto&family=Overpass+Mono&display=swap" rel="stylesheet"/>
+      <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Roboto+Mono&family=Overpass+Mono:wght@700&display=swap" rel="stylesheet"/>
       </Head>
       <body>
         <Main />
