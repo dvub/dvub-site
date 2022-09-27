7cea51fb48e3ffd2f06a0e75841f10d0a0153571
dvub <84478450+dvub@users.noreply.github.com>
2022-09-27 09:11:15 -0700

    work on about

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,39 +3,45 @@
 const About = () => {
   return (
     <div>
-    todo: what i hope to achieve, my current skills, my other passions, maybe some pictures and stuff.
-    maybe also break each section into its own component with props or something
-      <h1>About</h1>
-      <h2>Overview</h2>
-      <p>
-        My name is dvub. I am a student currently studying computer science. 
-        I build some projects for school, but most of the time I build projects for fun. (like this website.) <br/>
-        Coding is one of my passions, not only as a hobby, but as a way to make change in the world.
-        I believe computer science is essential to making change and making it a better place.
-      </p>
-      <hr/>
-      <h2>Background</h2>
-      <p>
-        For most of my life I was interested in technology. I always messed around with old hardware and was fascinated by it.
-        When I was younger I discovered Scratch, a simple, block-based programming language. I loved to build games and projects
-        of different sorts, and was fascinated and amazed by what people could do with such a tool. I then started really programming
-        because I wanted to write mods for the popular game, Terraria. From there, my passion for programming sparked, and now I hope to be
-        a professional developer someday.
-      </p>
-      <hr/>
-      <h2>Skills</h2>
-      <p>
-        I have about 2 years of Javascript and C# experience, and am currently learning TypeScript and Rust.
-      </p>
-      
-      <hr/>
-      <h2>Goals</h2>
-      <hr/>
-      a
-      <h2>Other Passions</h2>
-      <hr/>
-      
-
+      <div style={{width: '50%'}}>
+      todo: what i hope to achieve, my current skills, my other passions, maybe some pictures and stuff.
+      maybe also break each section into its own component with props or something
+        <h1>About</h1>
+        <h2>
+          Overview
+        </h2>
+        <p>
+          My name is dvub. I am a student currently studying computer science. 
+          I build some projects for school, but most of the time I build projects for fun. (like this website.) <br/>
+          Coding is one of my passions, not only as a hobby, but as a way to make change in the world.
+          I believe computer science is essential to making change and making it a better place.
+        </p>
+        <hr/>
+        <h2>
+          Background
+        </h2>
+        <p>
+          For most of my life I was interested in technology. I always messed around with old hardware and was fascinated by it.
+          When I was younger I discovered Scratch, a simple, block-based programming language. I loved to build games and projects
+          of different sorts, and was fascinated and amazed by what people could do with such a tool. I then started really programming
+          because I wanted to write mods for the popular game, Terraria. From there, my passion for programming sparked, and now I hope to be
+          a professional developer someday.
+        </p>
+        <hr/>
+        <h2>Skills</h2>
+        <p>
+          I have about 2 years of Javascript and C# experience, and am currently learning TypeScript and Rust.
+          I have about 1 year of full stack web development, using tools like Express, Nextjs, React, etc.
+          I have some experience with SQL databases as well.
+        </p>
+        
+        <hr/>
+        <h2>Goals</h2>
+        <hr/>
+        a
+        <h2>Other Passions</h2>
+        <hr/>
+      </div>
     </div>
   );
 }
