6bef288a98bde52093814774f19d61648d009277
dvub <84478450+dvub@users.noreply.github.com>
2022-09-26 20:37:52 -0700

    adjusted animation offsets, changed r3f canvas position

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,25 +6,24 @@ import Sphere from '../components/sphere'
 import { EffectComposer, Vignette, DepthOfField } from '@react-three/postprocessing'
 import { Container, Col, Row } from 'react-bootstrap'
 
-// SET CANVAS EL WIDTH AND HEIGHT INSIDE OF DIV
-
+// todos: 
 // work on readme
 
 const Home: NextPage = () => {
   return (
     <div>
       <div>
-        <Container> 
+        <Container>
           <Row xs={1} md={2}>
           <Col>
             <div>
               <h1 className='animate'>I&#39;m dvub, a</h1>
-              <h1 className='mono animate' style={{animationDelay: '1s'}}>
+              <h1 className='mono animate' style={{animationDelay: '0.5s'}}>
                   new developer();
                 </h1>
-              <h1 style={{animationDelay: '2s'}} className='animate'>Welcome.</h1>
+              <h1 style={{animationDelay: '1s'}} className='animate'>Welcome.</h1>
               <hr />
-              <p className='animate' style={{animationDelay: '2.5s'}}>
+              <p className='animate' style={{animationDelay: '1.5s'}}>
                 I&#39;m a student who enjoys building projects and just <b>making cool stuff</b>. 
                 I also love <b>opportunities to learn</b> new and interesting things. 
                 One day I hope to be a <b>professional developer</b> and make an <b>impact on the world</b> using technology.
@@ -34,11 +33,13 @@ const Home: NextPage = () => {
             </div>
           </Col>
           <Col>
-          <div>
-            <p>hover over me</p>
-            <Canvas camera={{ position: [-35, 0, 0]}} className='animate' style={{animationDelay: '2.5s', height: '25rem'}}>
+          <div style={{textAlign: 'center', animationDelay: '2s'}} className='animate'>
+            
+            <Canvas camera={{ position: [-35, 0, 0]}} style={{height: '25rem'}}>
+            
               <Sphere />
             </Canvas>
+            <p>(hover over me)</p>
             </div>
           </Col>
           </Row>
