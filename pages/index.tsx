366569b701f83b6b823e1d84fae76a00d46f0505
dvub <84478450+dvub@users.noreply.github.com>
2022-09-28 18:09:33 -0700

    removed imports, tweaked animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import type { NextPage } from 'next'
-import { Canvas, useThree } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import CSS from 'csstype'
+import { Canvas } from '@react-three/fiber'
 import Sphere from '../components/sphere'
-import { EffectComposer, Vignette, DepthOfField } from '@react-three/postprocessing'
 import { Container, Col, Row } from 'react-bootstrap'
 
 // todos: 
@@ -18,16 +15,14 @@ const Home: NextPage = () => {
             <Col>
               <div>
                 <h1 className='animate'>I&#39;m dvub, a</h1>
-                <h1 className='mono animate' style={{ animationDelay: '0.5s' }}>
+                <h1 className=' mono animate' style={{ animationDelay: '0.125s', maxWidth: '100%' }}>
                   new developer();
                 </h1>
-                <h1 style={{ animationDelay: '1s' }} className='animate'>Welcome.</h1>
-                <hr />
-                <div className='animate' style={{ animationDelay: '2s' }}>
+                <h1 style={{ animationDelay: '0.25s' }} className='animate'>Welcome.</h1>
+                <hr style={{ animationDelay: '0.375s' }} className='animate'/>
+                <div className='animate' style={{ animationDelay: '0.5s', maxWidth: '100%' }}>
                   <p>
-                    I&#39;m a student who enjoys building projects and just <b>making cool stuff</b>.
-                    I also love <b>opportunities to learn</b> new and interesting things.
-                    One day I hope to be a <b>professional developer</b> and make an <b>impact on the world</b> using technology.
+                    I&#39;m a student who enjoys building projects and just <b>making cool stuff</b>. I also love <b>opportunities to learn</b> new and interesting things. One day I hope to be a <b>professional developer</b> and make an <b>impact on the world</b> using technology.
                     <br />
                     If you want to read about some of my projects, you can look <a href='posts'>here.</a>
                   </p>
@@ -35,7 +30,7 @@ const Home: NextPage = () => {
               </div>
             </Col>
             <Col>
-              <div style={{ textAlign: 'center', animationDelay: '2.5s' }} className='animate'>
+              <div style={{ textAlign: 'center', animationDelay: '0.75s' }} className='animate'>
 
                 <Canvas camera={{ position: [-35, 0, 0] }} style={{ height: '25rem' }}>
 
