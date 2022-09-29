3138a9effb961f6a9d00bc35498527763d24617f
dvub <84478450+dvub@users.noreply.github.com>
2022-09-28 18:08:45 -0700

    minor style changes, indents

--- components/postCard.tsx
+++ components/postCard.tsx
@@ -15,7 +15,7 @@ const PostCard: FC<Props> = (props) => {
 
     let tagDisplay = tags ? tags.join(', ') : '';
     const styledTags = (
-        <span style={{ border: '4px solid #DADADA', backgroundColor: '#DADADA', borderRadius: '5px' }}>
+        <span style={{ border: '4px solid #DADADA', backgroundColor: '#DADADA', borderRadius: '5px', fontFamily: 'JetBrains Mono', fontWeight: '700' }}>
             {tagDisplay}
         </span>
     );
@@ -24,19 +24,28 @@ const PostCard: FC<Props> = (props) => {
         // /posts (slash at the front) for absolute path
         <Link href={`/posts/${fileName}`}>
             <Card style={{ border: 'transparent' }}>
+
                 <div style={{ height: '10rem', overflow: 'hidden', }}>
+
                     <Card.Body>
-                        <Card.Title className='mono'><h4><b>{title}</b></h4></Card.Title>
+                        <Card.Title className='mono' style={{ fontWeight: '700', fontSize: '24px' }}>{title}</Card.Title>
+
                         <Card.Text style={{ fontSize: '14px' }}>
                             {description}
                         </Card.Text>
+
                     </Card.Body>
                 </div>
                 <div style={{ height: '4rem' }}>
+
                     <Card.Footer style={{ backgroundColor: 'transparent', border: 'transparent', fontSize: '15px' }}>
+
                         {date} · {styledTags}
+
                     </Card.Footer>
+
                 </div>
+
             </Card>
         </Link>
     );
