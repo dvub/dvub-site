0ebf17533ccd29433c862609b926d202a48f7f3f
kaya <84478450+dvub@users.noreply.github.com>
2023-07-01 14:04:04 -0700

    add links metadata

--- types/metadata.ts
+++ types/metadata.ts
@@ -1,9 +1,10 @@
 export interface Metadata {
-    title: string;
-    author: string;
-    authorLink: string;
-    date: string;
-    description: string;
-    tags: Array<string>;
-    fileName: string;
+  title: string;
+  author: string;
+  authorLink: string;
+  date: string;
+  description: string;
+  tags: Array<string>;
+  fileName: string;
+  links: Array<string>;
 }
