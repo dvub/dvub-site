653caeefb12fe738f684cdf4010c89c0db1fafad
kaya <84478450+dvub@users.noreply.github.com>
2023-07-10 17:58:17 -0700

    work on comment form with react-boostrap

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
-import React, { FC, ReactNode, useEffect, useState } from "react";
+import React, { FC, FormEventHandler, ReactNode, useEffect, useState } from "react";
 import type { Metadata } from "../types/metadata";
 import Head from "next/head";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Button, Form, InputGroup } from "react-bootstrap";
 import PostCard from "./PostCard";
 
 interface Props {
@@ -12,20 +12,23 @@ interface Props {
 const Layout: FC<Props> = (props) => {
   const { children, meta } = props;
   const { title, author, date, authorLink, description, tags } = meta; // metadata of post
+
   const postCount = 3; // the number of posts to display on the sidebar
+  
   const [metas, setMetas] = useState<Metadata[]>([]);
   const [isLoading, setLoading] = useState(false); // set loading text for fetching data -- in this case, posts
   const [commentState, setCommentState] = useState({
     name: "",
     comment: "",
   });
-  const onFormUpdate = (e: React.FormEvent<HTMLInputElement>) => {
+  const onFormUpdate = (e: any) => {
     setCommentState({
       ...commentState,
       [e.currentTarget.name]: e.currentTarget.value,
     });
   };
-  const onFormSubmit = () => {
+  const onFormSubmit = (event: any) => {
+    event.preventDefault();
   };
 
   useEffect(() => {
@@ -37,26 +40,36 @@ const Layout: FC<Props> = (props) => {
         setLoading(false);
       });
   }, []);
-  const tagDisplay = tags ? tags.join(", ") : ""; // i forgot why i had to do this tbh
 
-  const listItems = isLoading ? "Loading" : metas.map((d) => {
-    const { title } = d;
-    // a wrapper div to add some margins
-    return (
-      <div style={{ margin: "1rem" }}>
-        <PostCard meta={d} key={title} />
-      </div>
-    );
-  });
+  const tagDisplay = tags ? tags.join(", ") : ""; // i forgot why i had to do this tbh
+  const listItems = isLoading
+    ? "Loading"
+    : metas.map((d) => {
+        const { title } = d;
+        // a wrapper div to add some margins
+        return (
+          <div style={{ margin: "1rem" }}>
+            <PostCard meta={d} key={title} />
+          </div>
+        );
+      });
+
+
+  const t = `${title} | Blog`; // for some reason, if this is inline in the <title>, it thinks you're rendering multiple titles
+  // so fuck that
 
   return (
     <div style={{ marginBottom: "10rem" }}>
       <Head>
-        <title>{title}</title>
+        <title>{t}</title>
         <link
           rel="stylesheet"
           href="https://unpkg.com/dracula-prism/dist/css/dracula-prism.css"
         />
+        <meta name="description" content={description} />
+        <meta name="author" content="dvub" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta content={tags.join(", ")}></meta>
       </Head>
       {/* the main content of the page is split vertically, with the post on the left and comments/posts on the right */}
       <Container>
@@ -68,14 +81,11 @@ const Layout: FC<Props> = (props) => {
                 <h1>{title}</h1>
                 <a href={authorLink}>{author}</a> · {date} · {tagDisplay}
                 <br />
-
                 <i>{description}</i>
               </header>
               <hr />
               {/* props.children will render the standard md content*/}
-              <div>
-                {children}
-              </div>
+              <div>{children}</div>
               thanks for reading,
               <br />
               <i>~dvub</i>
@@ -88,49 +98,42 @@ const Layout: FC<Props> = (props) => {
               <Row>
                 <h2>Other Posts</h2>
                 <Container>
-                  <Col>
-                    {listItems}
-                  </Col>
+                  <Col>{listItems}</Col>
                 </Container>
               </Row>
               <Row>
                 <h2>Comments</h2>
-                <form>
+                <Form onSubmit={onFormSubmit}>
                   <p>
-                    Write your own... NOTE: Comments are actively moderated.
+                    Write your own... <br /> NOTE: Comments are actively
+                    moderated.
                   </p>
-                  <label>Your Name</label>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Text>@</InputGroup.Text>
+                    <Form.Control
+                      placeholder="Username"
+                      aria-label="Username"
+                      required
+                      id="name"
+                      name="name"
+                    />
+                  </InputGroup>
+                  <InputGroup>
+                    <Form.Control
+                      as="textarea"
+                      name="comment"
+                      id="comment"
+                      aria-label="Comment"
+                      placeholder="Comment"
+                      required
+                      onChange={onFormUpdate}
+                    />
+                  </InputGroup>
                   <br />
-                  <input
-                    type="text"
-                    required
-                    name="name"
-                    id="name"
-                    value={commentState.name}
-                    placeholder="..."
-                    onChange={onFormUpdate}
-                    maxLength={30}
-                  >
-                  </input>
-                  <br />
-                  <label>Comment</label>
-                  <br />
-                  <input
-                    type="text"
-                    required
-                    name="comment"
-                    id="comment"
-                    placeholder="..."
-                    value={commentState.comment}
-                    onChange={onFormUpdate}
-                    maxLength={200}
-                    style={{
-                      minWidth: "100%",
-                      minHeight: "10rem",
-                    }}
-                  >
-                  </input>
-                </form>
+                  <Button variant="primary" type="submit">
+                    Post Comment
+                  </Button>
+                </Form>
               </Row>
             </Container>
           </Col>
