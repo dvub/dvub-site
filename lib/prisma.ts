176aa5d688569c3b1241c5dd0a18746436482442
kaya <84478450+dvub@users.noreply.github.com>
2023-07-13 21:36:28 -0700

    change comments

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from '@prisma/client';
 // so.... i copied this from a vercel guide on fullstack apps with prisma
 // i cant tell if im just too stupid to understand this,,
 // or if its just genuinely shit code
-// help me please
 
 let prisma: PrismaClient;
 
