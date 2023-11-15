582b55735a1165a669cce737c4bd1d2cb67ad129
dvub <dvubdevs@gmail.com>
2023-11-14 20:58:31 -0800

    Formatting Changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,48 +1,46 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from '../components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from '../components/Navbar'
 import CSS from 'csstype'
-import { Analytics } from '@vercel/analytics/react';
-import Footer from '../components/Footer';
+import { Analytics } from '@vercel/analytics/react'
+import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const footerHeight = '4rem'
+	const margins = '5%'
+	const style: CSS.Properties = {
+		marginRight: margins,
+		marginLeft: margins,
+		marginTop: '2rem',
+		paddingBottom: footerHeight,
+	}
+	const footerStyle: CSS.Properties = {
+		position: 'absolute',
+		bottom: 0,
+		width: '100%',
+		height: footerHeight,
+		fontFamily: 'Overpass Mono',
+		textAlign: 'center',
+		lineHeight: footerHeight,
+	}
+	const wrapperStyle: CSS.Properties = {
+		position: 'relative',
+		minHeight: '100vh',
+	}
 
-
-  const footerHeight = '4rem';
-  const margins = '5%';
-  const style: CSS.Properties = {
-    marginRight: margins,
-    marginLeft: margins,
-    marginTop: '2rem',
-    paddingBottom: footerHeight,
-  };
-  const footerStyle: CSS.Properties = {
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
-    height: footerHeight,
-    fontFamily: 'Overpass Mono',
-    textAlign: 'center',
-    lineHeight: footerHeight,
-  }
-  const wrapperStyle: CSS.Properties = {
-    position: 'relative',
-    minHeight: '100vh',
-  }
-
-  return (
-    <div style={wrapperStyle}>
-      <Analytics/>
-      <NavBar />
-      <div style={style}>
-        <Component {...pageProps} ></Component>
-      </div>
-      <div style={footerStyle}>
-        <Footer />
-      </div>
-    </div>
-  );
+	return (
+		<div style={wrapperStyle}>
+			<Analytics />
+			<NavBar />
+			<div style={style}>
+				<Component {...pageProps}></Component>
+			</div>
+			<div style={footerStyle}>
+				<Footer />
+			</div>
+		</div>
+	)
 }
 
 export default MyApp
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,71 +1,146 @@
 import Image from 'next/image'
 import Head from 'next/head'
 const About = () => {
-  const imgSize = 64;
+	const imgSize = 64
 
-  return (
+	return (
+		<div style={{ maxWidth: '40rem' }}>
+			<Head>
+				<title>About | Dvub</title>
+				<meta
+					name='description'
+					content="I'm dvub; Learn a little bit about me!"
+				/>
+				<meta name='author' content='dvub' />
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1.0'
+				/>
+			</Head>
+			<h1 className='animate'>About</h1>
+			<hr />
+			<div style={{ marginBottom: '5rem' }}>
+				<div
+					className='animate'
+					style={{ animationDelay: '0.125s', maxWidth: '100%' }}>
+					<h2>Overview</h2>
+					<p>
+						My name is <b>dvub</b>. I am a student currently
+						studying computer science. I build some projects for
+						school, but most of the time I build projects for fun.
+						(like this website) <b>Coding is one of my passions</b>;
+						I not only see it as a hobby, but as a way to make
+						change in the world.
+					</p>
+					<hr />
+				</div>
+				<div
+					className='animate'
+					style={{ animationDelay: '0.25s', maxWidth: '100%' }}>
+					<h2>Background</h2>
+					<p>
+						For most of my life, I was interested in technology. I
+						always messed around with old hardware and was
+						fascinated by it. When I was younger I discovered
+						Scratch, a simple, block-based programming language. I
+						loved to build games and projects of different sorts,
+						and was fascinated and amazed by what people could do
+						with such a tool. I then started really programming
+						because I wanted to write mods for the popular game,
+						Terraria. From there, my passion for programming
+						sparked, and now I hope to be a professional developer
+						someday.
+					</p>
 
-    <div style={{ maxWidth: '40rem' }}>
-      <Head>
-        <title>About | Dvub</title>
-        <meta name="description" content="I'm dvub; Learn a little bit about me!" />
-        <meta name="author" content="dvub" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <h1 className='animate'>About</h1>
-      <hr />
-      <div style={{ marginBottom: '5rem' }}>
-        <div className='animate' style={{ animationDelay: '0.125s', maxWidth: '100%' }}>
-          <h2>
-            Overview
-          </h2>
-          <p>
-            My name is <b>dvub</b>. I am a student currently studying computer science. I build some projects for school, but most of the time I build projects for fun. (like this website) <b>Coding is one of my passions</b>; I not only see it as a hobby, but as a way to make change in the world.
-          </p>
-          <hr />
-        </div>
-        <div className='animate' style={{ animationDelay: '0.25s', maxWidth: '100%' }}>
-          <h2>
-            Background
-          </h2>
-          <p>
-            For most of my life, I was interested in technology. I always messed around with old hardware and was fascinated by it. When I was younger I discovered Scratch, a simple, block-based programming language. I loved to build games and projects of different sorts, and was fascinated and amazed by what people could do with such a tool. I then started really programming because I wanted to write mods for the popular game, Terraria. From there, my passion for programming sparked, and now I hope to be a professional developer someday.
-          </p>
+					<hr />
+				</div>
+				<div
+					className='animate'
+					style={{ animationDelay: '0.375s', maxWidth: '100%' }}>
+					<h2>Skills</h2>
 
-          <hr />
-        </div>
-        <div className='animate' style={{ animationDelay: '0.375s', maxWidth: '100%' }}>
-          <h2>Skills</h2>
-
-          <div>
-            <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" width={imgSize} height={imgSize} alt={''} />
-            <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" width={imgSize} height={imgSize} alt={''} />
-            <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" width={imgSize} height={imgSize} alt={''} />
-            <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rust/rust-plain.svg" width={imgSize} height={imgSize} alt={''} />
-            <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" width={imgSize} height={imgSize} alt={''} />
-            <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" width={imgSize} height={imgSize} alt={''} />
-
-
-          </div>
-          <p>
-            I have about 2 years of Javascript and C# experience, and am currently learning TypeScript and Rust. I have about 1 year of full stack web development experience, using tools like Expressjs, Nextjs, React, etc. I have some experience with SQL databases as well.
-          </p>
-          <hr />
-        </div>
-        <div className='animate' style={{ animationDelay: '0.5s', maxWidth: '100%' }}>
-          <h2>Goals</h2>
-          Currently, my goal is to get through school and go to a good university. (and get through that, too) From there, I hope to find a job as a developer of some sort, maybe for web development, or for something else. I want to keep building projects and code as a hobby throughout this as well. In terms of development, I would like to work on more back-end projects. I would like to improve my webdev skills as well. I would like to learn more about Rust and build more projects with it. I would also like to learn other low-level languages, such as C++, at some point. <b>Above all, I want to learn more everyday and improve my skills.</b>
-          <hr />
-        </div>
-        <div className='animate' style={{ animationDelay: '0.625s', maxWidth: '100%' }}>
-          <h2>Other Passions</h2>
-          <p>
-            Other than programming, I have many other hobbies and passions. I love to play video games and play, as well as listen to music! I&apos;ve played piano and guitar for a good portion of my life, and I absolutely love music. I quite like to play jazz!
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+					<div>
+						<Image
+							src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
+							width={imgSize}
+							height={imgSize}
+							alt={''}
+						/>
+						<Image
+							src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg'
+							width={imgSize}
+							height={imgSize}
+							alt={''}
+						/>
+						<Image
+							src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
+							width={imgSize}
+							height={imgSize}
+							alt={''}
+						/>
+						<Image
+							src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rust/rust-plain.svg'
+							width={imgSize}
+							height={imgSize}
+							alt={''}
+						/>
+						<Image
+							src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg'
+							width={imgSize}
+							height={imgSize}
+							alt={''}
+						/>
+						<Image
+							src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg'
+							width={imgSize}
+							height={imgSize}
+							alt={''}
+						/>
+					</div>
+					<p>
+						I have about 2 years of Javascript and C# experience,
+						and am currently learning TypeScript and Rust. I have
+						about 1 year of full stack web development experience,
+						using tools like Expressjs, Nextjs, React, etc. I have
+						some experience with SQL databases as well.
+					</p>
+					<hr />
+				</div>
+				<div
+					className='animate'
+					style={{ animationDelay: '0.5s', maxWidth: '100%' }}>
+					<h2>Goals</h2>
+					Currently, my goal is to get through school and go to a good
+					university. (and get through that, too) From there, I hope
+					to find a job as a developer of some sort, maybe for web
+					development, or for something else. I want to keep building
+					projects and code as a hobby throughout this as well. In
+					terms of development, I would like to work on more back-end
+					projects. I would like to improve my webdev skills as well.
+					I would like to learn more about Rust and build more
+					projects with it. I would also like to learn other low-level
+					languages, such as C++, at some point.{' '}
+					<b>
+						Above all, I want to learn more everyday and improve my
+						skills.
+					</b>
+					<hr />
+				</div>
+				<div
+					className='animate'
+					style={{ animationDelay: '0.625s', maxWidth: '100%' }}>
+					<h2>Other Passions</h2>
+					<p>
+						Other than programming, I have many other hobbies and
+						passions. I love to play video games and play, as well
+						as listen to music! I&apos;ve played piano and guitar
+						for a good portion of my life, and I absolutely love
+						music. I quite like to play jazz!
+					</p>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default About
