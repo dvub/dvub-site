411fee367c6954182c3a3e728bd9df74dfa2876c
dvub <dvubdevs@gmail.com>
2023-11-21 15:03:00 -0800

    Add path.resolve()

--- pages/api/metas.ts
+++ pages/api/metas.ts
@@ -1,11 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import type { Metadata } from '../../types/metadata';
 import fs from 'fs';
+import path from 'path';
 
 const metas = async (req: NextApiRequest, res: NextApiResponse) => {
 	const metadatas: Metadata[] = [];
 	try {
-		const postFiles = fs.readdirSync('./pages/blog');
+		const blogPath = path.resolve(process.cwd(), './pages/blog/');
+
+		const postFiles = fs.readdirSync(blogPath);
 		for (let post of postFiles) {
 			const { meta } = await import(`../blog/${post}`); // dynamic imports
 
