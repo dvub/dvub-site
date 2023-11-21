c928ecd7402ab344943421a8e7082f62bccd8b7b
dvub <dvubdevs@gmail.com>
2023-11-21 14:53:28 -0800

    Refactor error handling

--- pages/api/metas.ts
+++ pages/api/metas.ts
@@ -1,28 +1,21 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import type { Metadata } from '../../types/metadata'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { Metadata } from '../../types/metadata';
 import fs from 'fs';
 
 const metas = async (req: NextApiRequest, res: NextApiResponse) => {
+	const metadatas: Metadata[] = [];
+	try {
+		const postFiles = fs.readdirSync('./pages/blog');
+		for (let post of postFiles) {
+			const { meta } = await import(`../blog/${post}`); // dynamic imports
 
-    const postInfos: Metadata[] = [];
-    const postFiles = fs.readdirSync('./pages/blog');
-
-    for (let post of postFiles) {
-        try {
-            const { meta } = await import(`../blog/${post}`); // dynamic imports
-
-            postInfos.push(meta);
-
-        } catch (error) {
-            // if we have an error, just stop and don't return any data..
-
-            console.log(error);
-            return;
-        }
-        
-        res.status(200).json(postInfos)
-    }
-
+			metadatas.push(meta);
+		}
+		res.status(200).json(metadatas);
+	} catch (error) {
+		console.log(error);
+		return;
+	}
 };
 
-export default metas
+export default metas;
